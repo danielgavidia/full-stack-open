a08a19309ce67fba50d7db5d7d31db794e3d2e37
Daniel Gavidia <dgavidia1@gmail.com>
2024-05-16 22:14:42 -0400

    added root app for part D exercises

--- courseinfo/src/main.jsx
+++ courseinfo/src/main.jsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import AppPartD from "./AppPartD.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<AppPartD />);
