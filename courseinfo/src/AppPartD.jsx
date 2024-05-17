5a4ad30055254fa10300a42339aa359d362937a7
Daniel Gavidia <dgavidia1@gmail.com>
2024-05-16 22:45:32 -0400

    section: conditional rendering

--- courseinfo/src/AppPartD.jsx
+++ courseinfo/src/AppPartD.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const AppPartD = () => {
+export const AppPartD = () => {
   const [clicks, setClicks] = useState({ left: 0, right: 0 });
   const handleLeftClick = () => {
     setClicks({ ...clicks, left: clicks.left + 1 });
@@ -48,4 +48,39 @@ export const AppPartD2 = () => {
   );
 };
 
-export default AppPartD;
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+export const AppPartD3 = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+  };
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
