011f6f18ade36cd4fb3aa49c26f8e86a4a71e1f6
Daniel Gavidia <dgavidia1@gmail.com>
2024-05-17 14:37:56 -0400

    added button component and good, netrual and bad states

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
+import { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  console.log(good);
+  const [neutral, setNeutral] = useState(0);
+  console.log(neutral);
+  const [bad, setBad] = useState(0);
+  console.log(bad);
   return (
     <>
-      <div>App</div>
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text={"good"} />
+        <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+        <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+        <h1>statistics</h1>
+      </div>
     </>
   );
-}
+};
 
 export default App;
