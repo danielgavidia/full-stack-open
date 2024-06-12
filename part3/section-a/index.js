02229a27297ef41e42c92cbea3990b95440f9136
Daniel Gavidia <dgavidia1@gmail.com>
2024-06-12 19:29:18 -0400

    added VS code REST client

--- part3/section-a/index.js
+++ part3/section-a/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
     {
         id: 1,
@@ -25,6 +27,31 @@ app.get("/", (request, response) => {
 
 app.get("/api/notes", (request, response) => {
     response.json(notes);
+});
+
+// get
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find((note) => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+// post
+app.post("/api/notes", (request, response) => {
+    const note = request.body;
+    console.log(note);
+    response.json(note);
+});
+
+// delete
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+    response.status(204).end();
 });
 
 const PORT = 3001;
