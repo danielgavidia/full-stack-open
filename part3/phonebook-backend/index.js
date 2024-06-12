bc36dbf92e09327e604b56b62f278b0b7df75df0
Daniel Gavidia <dgavidia1@gmail.com>
2024-06-12 19:47:18 -0400

    added index.js

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,33 @@
+// load express
+const express = require("express");
+const app = express();
+
+// data
+[
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
+];
+
+// initialize application
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
