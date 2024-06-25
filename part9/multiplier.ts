7fe3a93d5a5e0e46f815208917426a9899ca0c46
Daniel Gavidia <dgavidia1@gmail.com>
2024-06-24 20:24:13 -0400

    rearranged directories

--- part9/multiplier.ts
+++
@@ -1,7 +0,0 @@
-type Operation = "multiply" | "add" | "divide";
-
-const multiplicator = (a: number, b: number, printText: string) => {
-    console.log(printText, a * b);
-};
-
-multiplicator(2, 4, "Multiplied numbers 2 and 4, result is:");
